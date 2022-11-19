00693d3423c04b3b5a89d5709e1bf9583e5562fe
sparkyruth <34682552+sparkyruth@users.noreply.github.com>
2022-11-19 12:10:12 +0300

    add categories, filter products by categories

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,27 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { AnyForUntypedForms } from '@angular/forms';
-import {veg_products, fruit_products, meat_products, dairy_products, baked_products} from '../../utils/constants'
+import { veg_products, fruit_products, meat_products, dairy_products, baked_products } from '../../utils/constants'
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  allProducts:any
-  category_products:any
-  shuffled_products:any
+  allProducts: any
+  categories = [
+    { img: "../../../assets/images/categories_header/cat-1.png", name: "fruits" },
+    { img: "../../../assets/images/categories_header/cat-2.png", name: "veggies" },
+    { img: "../../../assets/images/categories_header/cat-4.png", name: "meat" },
+    { img: "../../../assets/images/categories_header/cat-5.png", name: "baked" }
+
+  ]
+  shuffled_products: any
+  category_products: any
+  shuffled_categories: any
+  showCategory: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
-    console.log(typeof veg_products)
     //create an array and push all the products
-    this.allProducts=[]
+    this.allProducts = []
     // this.allProducts.push(veg_products,fruit_products, meat_products, dairy_products, baked_products)
-    this.allProducts.push(...veg_products,...fruit_products, ...meat_products,...dairy_products,...baked_products)
-    console.log(this.allProducts, typeof this.allProducts);
-    this.shuffled_products = this.allProducts.sort( () => .5 - Math.random() )
-    console.log(this.shuffled_products)     
-    
+    this.allProducts.push(...veg_products, ...fruit_products, ...meat_products, ...dairy_products, ...baked_products)
+    this.shuffled_products = this.allProducts.sort(() => .5 - Math.random())
+    this.shuffled_categories = this.categories.sort(() => .5 - Math.random())
+console.log(typeof this.shuffled_products)
+  }
+
+  show_category(category: any) {
+    console.log(category)
+    this.showCategory = true
+
+    if (category === 'fruits') {
+      this.category_products=fruit_products
+      console.log(this.category_products, typeof this.category_products);
+    } else if (category === 'veggies') {
+      this.category_products=veg_products
+      console.log(this.category_products);
+    } else if (category === 'meat') {
+      this.category_products=meat_products
+      console.log(this.category_products);
+    } else if (category === 'baked') {
+      this.category_products=baked_products
+      console.log(this.category_products);
+    }
+
   }
 
 }
